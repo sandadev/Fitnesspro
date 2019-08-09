669de4d489c900f4a0110aa3b458b95d264ab896
sandadev <santhoshkumartalari@gmail.com>
2019-08-09 20:31:04 +0530

    added shared layout and service layout

---
+++ Fitnesspro.Web/Controllers/ServiceController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fitnesspro.Web.Controllers
+{
+    public class ServiceController : Controller
+    {
+        //
+        // GET: /Service/
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
